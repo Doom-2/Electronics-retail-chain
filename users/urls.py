df8506309dac089ba79451e7d0fdf174e1691e37
doom <phramov945@gmail.com>
2023-03-08 02:31:52 +0300

    add slashes (/) at the end of routes

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('signup', views.UserCreateView.as_view(), name='create-user'),
-    path('login', views.UserLoginView.as_view(), name='login-user'),
-    path('profile', views.UserProfile.as_view(), name='user-profile'),
-    path('logout', views.UserLogout.as_view(), name='user-profile'),
+    path('signup/', views.UserCreateView.as_view(), name='create-user'),
+    path('login/', views.UserLoginView.as_view(), name='login-user'),
+    path('profile/', views.UserProfile.as_view(), name='user-profile'),
+    path('logout/', views.UserLogout.as_view(), name='user-profile'),
 ]
